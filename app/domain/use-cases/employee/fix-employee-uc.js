8ce8ba1fbf9a2d55b44dd3d4b5175a2ca7f3ad7f
Hector Gomez <hector.gomez.varela@mobgen.com>
2019-05-30 17:18:06 +0200

    Init implementation

--- app/domain/use-cases/employee/fix-employee-uc.js
+++ app/domain/use-cases/employee/fix-employee-uc.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Joi = require('@hapi/joi');
+const { EmployeeRepository } = require('../../repositories');
+
 /**
  * Fixes an employee by changing the email and increasing the salary.
  * @param {String} id employee id.
@@ -7,7 +10,22 @@
  * @throws Error if the operation fails.
  */
 async function fixEmployeeCommand (id) {
-  return { result: 'To be implemented' };
+  await Joi.validate(id, Joi.string().required().label('id'));
+
+  const employee = await EmployeeRepository.findById(id);
+  const {
+    email,
+    salary,
+  } = employee;
+
+  const fixedEmail = email.replace('accenture', 'mobgen');
+  const fixedSalary = salary * 2;
+
+  return EmployeeRepository.updateEmailAndSalary(
+    id,
+    fixedEmail,
+    fixedSalary,
+  );
 }
 
 module.exports = fixEmployeeCommand;
