5b1d3707c0df4a04aff7ee678b41db1840ee4895
Hector Gomez <hector.gomez.varela@mobgen.com>
2019-05-30 17:02:07 +0200

    Add behaviour tests

--- __tests__/domain/use-cases/employee/fix-employee-uc.test.js
+++ __tests__/domain/use-cases/employee/fix-employee-uc.test.js
@@ -5,8 +5,14 @@ const { EmployeeRepository } = require('../../../../app/domain/repositories');
 
 const EMPLOYEE = {
   id: 'id',
-  email: 'employee@email.com',
-  salary: 10000000,
+  email: 'employee@accenture.com',
+  salary: 1000000,
+};
+
+const FIXED_EMPLOYEE = {
+  id: 'id',
+  email: 'employee@mobgen.com',
+  salary: 2000000,
 };
 
 describe('[use-cases-tests] [fix-employee]', () => {
@@ -33,24 +39,38 @@ describe('[use-cases-tests] [fix-employee]', () => {
   describe('[business logic tests]', () => {
     beforeEach(() => {
       EmployeeRepository.findById = jest.fn(() => EMPLOYEE);
+      EmployeeRepository.updateEmailAndSalary = jest.fn(() => FIXED_EMPLOYEE);
     });
 
     afterEach(() => {
       EmployeeRepository.findById.mockClear();
+      EmployeeRepository.updateEmailAndSalary.mockClear();
     });
 
     it("should fail if the repository can't find the employee", async (done) => {
       EmployeeRepository.findById = jest.fn(() => null);
       try {
-        await fixEmployeeCommand('whatever');
+        await fixEmployeeCommand('someId');
         done.fail();
       } catch (err) {
         expect(err).toMatchObject({
           name: 'Error',
           message: 'Employee not found',
         });
+        expect(EmployeeRepository.findById).toHaveBeenCalledTimes(1);
+        expect(EmployeeRepository.findById).toHaveBeenCalledWith('someId');
         done();
       }
+    });
+
+    it('should call repository with the fixed employee and return it', async () => {
+      const employee = await fixEmployeeCommand('someId');
+      expect(employee).toEqual(FIXED_EMPLOYEE);
+      expect(EmployeeRepository.findById).toHaveBeenCalledTimes(1);
+      expect(EmployeeRepository.findById).toHaveBeenCalledWith('someId');
+      expect(EmployeeRepository.updateEmailAndSalary).toHaveBeenCalledTimes(1);
+      expect(EmployeeRepository.updateEmailAndSalary)
+        .toHaveBeenCalledWith('someId', FIXED_EMPLOYEE.email, FIXED_EMPLOYEE.salary);
     });
   });
 });
